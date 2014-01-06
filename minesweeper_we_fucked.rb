8a6b08435d4b6558639855d6afc316c6168a913d
App Academy Student <student@appacademy.io>
2014-01-06 14:31:46 -0500

    Small refactor

--- minesweeper_we_fucked.rb
+++ minesweeper_we_fucked.rb
@@ -1,4 +1,9 @@
+require 'debugger'
+
+
+
 class Minesweeper
+
   attr_accessor :solution_board, :user_board
 
   def initialize
@@ -9,14 +14,14 @@ class Minesweeper
 
   def play
     @solution_board.seed_bombs
+    @solution_board.add_numbers_to_board
+
     loop do
       @user_board.display
 
       user_input, x, y = @user.prompt
+      action(user_input, x, y)
 
-      reveal(x,y) if user_input == 'R'
-      flag(x,y) if user_input == 'F'
-      unflag(x,y) if user_input == 'U'
       if lose?(x,y)
         @solution_board.display
         puts "You lose idiot!"
@@ -26,11 +31,15 @@ class Minesweeper
     end
   end
 
+  def action(user_input, x, y)
+    reveal(x,y) if user_input == 'R'
+    flag(x,y) if user_input == 'F'
+    unflag(x,y) if user_input == 'U'
+  end
+
   def lose?(x,y)
     @user_board[[x,y]] == 'X'
   end
-
-
 
   def won?
     @solution_board.total_nums == @user_board.total_nums
@@ -44,11 +53,6 @@ class Minesweeper
     @user_board.rows[x][y] = "*"
   end
 
-  def game_over
-    @user_board.display
-    puts "You lose"
-  end
-
   def reveal(x,y)
     @user_board[[x,y]] = @solution_board[[x,y]]
 
@@ -56,11 +60,11 @@ class Minesweeper
     already_checked = []
 
     until queue.empty?
-
       neighbor_pos = queue.shift
+      already_checked << neighbor_pos
+
       next if already_checked.include?(neighbor_pos)
       next if @user_board[neighbor_pos] == 'F'
-      already_checked << neighbor_pos
 
       if @solution_board[neighbor_pos].is_a?(Fixnum)
         @user_board[neighbor_pos] = @solution_board[neighbor_pos]
@@ -69,76 +73,64 @@ class Minesweeper
         @user_board[neighbor_pos] = "_"
 
         neighbors = Board.get_neighbors([neighbor_pos[0],neighbor_pos[1]])
-        queue += neighbors.reject {|el| already_checked.include?(el) || queue.include?(el)}
 
+        #neighbors = Board.get_neighbors([neighbor_pos[0],neighbor_pos[1]])
+        queue += neighbors.reject {|el| already_checked.include?(el) || queue.include?(el)}
       end
     end
-    #next if bomb.
-  end
-
-
-  def game_over
-    puts 'You lose'
   end
 
 end
 
 class User
-
   def prompt
-    puts "The fuck u wanna do?"
+    puts "What would you like to do? (R)eveal, (F)lag, (U)nflag"
+    puts "For instance: R 3,5"
     input = gets.chomp.split(" ")
     user_action = input[0]
     x,y = input[1].split(",").map(&:to_i)
 
     [user_action, x, y]
   end
-
-
 end
 
-
-
 class Board
-  ROWS = 5
+  ROWS = 9
   attr_accessor :rows, :total_nums
 
   def self.blank_grid
-    Array.new(ROWS) {Array.new(ROWS) {"*"}} #then does this make sense?
+    Array.new(ROWS) {Array.new(ROWS) {"*"}}
   end
 
   def initialize(rows = self.class.blank_grid)
-    #this is in Minesweeper class. not board class irgh tnow.
     @rows = rows
     @total_nums = 0
   end
 
   def display
-    p @rows
+    @rows.each { |row|  puts row.join(" ") }
   end
 
   def seed_bombs
-    #how do we mark the bombs? just like B?
     seeded_bombs = 0
 
-    until seeded_bombs == 1
-      ran_x = rand(ROWS)
-      ran_y = rand(ROWS)
+    until seeded_bombs == ((ROWS*ROWS)/(ROWS*1.1)).round
+      ran_x, ran_y = rand(ROWS), rand(ROWS)
 
       if @rows[ran_x][ran_y] == '*'
         @rows[ran_x][ran_y] = 'X'
         seeded_bombs += 1
       end
     end
-
-    add_nums
   end
 
-  def add_nums
+  def add_numbers_to_board
     @rows.each_index do |i|
       @rows.each_index do |j|
         next if @rows[i][j] == 'X'
+
         num_bombs = count_neighboring_bombs([i,j])
+
         if num_bombs > 0
           @rows[i][j] = num_bombs
           @total_nums += 1
@@ -147,35 +139,39 @@ class Board
     end
   end
 
-  def self.get_neighbors(pos)
-    [[0,1], [0,-1], [1,1], [-1,1],
-    [1,0], [-1,0], [-1,-1], [1,-1]
-  ].map {|elem| [elem[0]+pos[0], elem[1]+pos[1]]}.delete_if {|el| Board.out_of_bounds?(el)}
-end
-
-def count_neighboring_bombs(pos) #[x,y]
-  neighbors = Board.get_neighbors(pos)
-
-  num_bombs = 0
-  neighbors.each do |neighbor_pos|
-    num_bombs += 1 if self[neighbor_pos] == 'X'
+  def self.get_neighbors(pos) #Doesn't include out of bounds.
+    [[0,1], [0,-1], [1,1], [-1,1],[1,0], [-1,0], [-1,-1], [1,-1]].map do |elem|
+      [elem[0]+pos[0], elem[1]+pos[1]].delete_if {|el| Board.out_of_bounds?(el)}
+    end
   end
-  num_bombs
+
+  def count_neighboring_bombs(pos)
+    neighbors = Board.get_neighbors(pos)
+    num_bombs = 0
+
+    neighbors.each do |neighbor_pos|
+      num_bombs += 1 if self[neighbor_pos] == 'X'
+    end
+
+    num_bombs
+  end
+
+  def self.out_of_bounds?(pos)
+    pos[0] < 0 || pos[0] == ROWS || pos[1] < 0 || pos[1] == ROWS
+  end
+
+  def [](pos)
+    x, y = pos[0], pos[1]
+    @rows[x][y]
+  end
+
+  def []=(pos, value)
+    x, y = pos[0], pos[1]
+    value = (value == "*" ? "_" : value)
+    @rows[x][y] = value
+  end
 end
 
-def self.out_of_bounds?(pos)
-  pos[0] < 0 || pos[0] == ROWS || pos[1] < 0 || pos[1] == ROWS
-end
 
-def [](pos)
-  x, y = pos[0], pos[1]
-  @rows[x][y]
-end
-
-def []=(pos, value)
-  x, y = pos[0], pos[1]
-  value = (value == "*" ? "_" : value)
-  @rows[x][y] = value
-end
-
-end
+g = Minesweeper.new
+g.play
